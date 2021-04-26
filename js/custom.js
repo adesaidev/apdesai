fb46cd2781c3c751a02ae4b023294d074a415100
Ashvin Desai <ashvindesai@live.com>
2021-04-26 18:22:14 +0530

    add contact form

--- js/custom.js
+++ js/custom.js
@@ -270,4 +270,25 @@ $(function(){
         }, 400);
     });
 
-});
+}); 
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('elaunch.ashvin@gmail.com');
+})();
+ 
+ 
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+ 
